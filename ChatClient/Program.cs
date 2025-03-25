089ac0f3a7c98741649f067da2c26ffecf32b51e
ElizabethBrown309 <annabear0909@gmail.com>
2025-03-25 17:28:23 +1000

    Started the fix

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+
+var factory = new ConnectionFactory{ HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare
+    (Queue: "Queue1",
+    durable: false, 
+    autoDelete: false,
+    arguments: null);
+
+Console.WriteLine("Enter messages to send. Type 'exit' to quit.");
+
+while (true)
+{
+    Console.Write("You: ");
+    string message = Console.ReadLine();
+
+    if (message.ToLower() == "exit")
+        break; 
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "", routingKey: "Queue1", basicProperties: null, body: body);
+
+    Console.WriteLine($"Sent message: {message}");
+}
